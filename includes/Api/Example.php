9884d2ab8d3d2dd79e82291eeec4b1d7df9d22c2
ryntab <57367794+ryntab@users.noreply.github.com>
2021-04-29 20:17:41 -0400

    Updated api endpoint with merged database tables

--- includes/Api/Example.php
+++ includes/Api/Example.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Api;
 
 use WP_REST_Controller;
@@ -6,12 +7,14 @@ use WP_REST_Controller;
 /**
  * REST_API Handler
  */
-class Example extends WP_REST_Controller {
+class Example extends WP_REST_Controller
+{
 
     /**
      * [__construct description]
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->namespace = 'dealer/api';
         $this->rest_base = 'orders';
     }
@@ -21,15 +24,16 @@ class Example extends WP_REST_Controller {
      *
      * @return void
      */
-    public function register_routes() {
+    public function register_routes()
+    {
         register_rest_route(
             $this->namespace,
             '/' . $this->rest_base,
             array(
                 array(
                     'methods'             => \WP_REST_Server::READABLE,
-                    'callback'            => array( $this, 'get_dealer_orders' ),
-                    'permission_callback' => array( $this, 'get_orders_permissions_check' ),
+                    'callback'            => array($this, 'get_dealer_orders'),
+                    'permission_callback' => array($this, 'get_orders_permissions_check'),
                     'args'                => $this->get_collection_params(),
                 )
             )
@@ -43,15 +47,33 @@ class Example extends WP_REST_Controller {
      *
      * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
      */
-    public function get_dealer_orders( $request ) {
+    public function get_dealer_orders($request)
+    {
         global $wpdb;
-        $order = $wpdb->get_results($wpdb->prepare("SELECT * FROM `wp_dealer_customers`"));
+        $array1 = $wpdb->get_results($wpdb->prepare("SELECT * FROM `wp_dealer_customers`"));
+        $array2 = $wpdb->get_results($wpdb->prepare("SELECT * FROM `wp_user_warranties`"));
+
+        // $arr2 = array_column($array2, "order_id");
+
+        // $finalArray = array();
+        // foreach ($array1 as $arr) {
+            // $key = array_search($arr["order_id"], $arr2);
+            // if ($key === false) {
+                // $key = array_search(0, $arr2);
+            // }
+            // unset($array2[$key]["order_id"]);
+            // $finalArray[] =     array_merge($arr, $array2[$key]);
+        // }
+
+
+
 
         $data['page'] = 1;
-        $data['results'] = $order;
+        $data['results'] = array_merge($array1, $array2);;
+
         $data['total_pages'] = 1;
         $data['total_results'] = 4;
-        
+
 
 
 
@@ -65,7 +87,8 @@ class Example extends WP_REST_Controller {
      *
      * @return true|WP_Error True if the request has read access, WP_Error object otherwise.
      */
-    public function get_orders_permissions_check( $request ) {
+    public function get_orders_permissions_check($request)
+    {
         return true;
     }
 
@@ -74,7 +97,8 @@ class Example extends WP_REST_Controller {
      *
      * @return array Collection parameters.
      */
-    public function get_collection_params() {
+    public function get_collection_params()
+    {
         return [];
     }
 }
