e1c5c041549ea9ee03451f9c39dbe3edab401696
ryntab <57367794+ryntab@users.noreply.github.com>
2021-05-03 12:17:27 -0400

    Switch customer detail display between old version and new version
    - Show old version of customer details if there is not data set in the database for the order.

--- includes/Order.php
+++ includes/Order.php
@@ -10,55 +10,38 @@ class Order
 {
     function __construct()
     {
-        add_action('woocommerce_admin_order_data_after_billing_address', [$this, 'render_admin_order_fields'], 5);
         add_action('add_meta_boxes', [$this, 'render_admin_order_meta_box']);
-    }
-
-    //Display Order Meta [Old Version]
-    public function render_admin_order_fields($order)
-    {
-        if ((get_post_meta($order->get_id(), 'customer_first_name', true))) {
-            echo '<h3>Dealer Submitted Customer Details</h3><div class="address">';
-            echo '<p><strong>Customer Name: </strong>' . get_post_meta($order->get_id(), 'customer_first_name', true) . ' ' . get_post_meta($order->get_id(), 'customer_last_name', true) . '</p>';
-            echo '<p><strong>Email Adress: </strong>' . get_post_meta($order->get_id(), 'customer_email', true) . '</p>';
-            echo '<p><strong>Address: </strong>' .
-                get_post_meta($order->get_id(), 'customer_country', true) . '<br>' .
-                get_post_meta($order->get_id(), 'customer_street_address', true) . '<br>' .
-                get_post_meta($order->get_id(), 'customer_town', true) . ', ' .
-                get_post_meta($order->get_id(), 'customer_state', true)  .  ' ' .
-                get_post_meta($order->get_id(), 'customer_zip', true) . ' ' .
-                '</p>';
-            echo '</div>';
-            return $order;
-        }
     }
 
     //Admin Order: Add metabox to order view
     public function render_admin_order_meta_box()
     {
-        add_meta_box('woocommerce-customer-details', __('Dealers Customer'), [$this, 'render_customer_details_meta_box'], 'shop_order', 'normal', 'high');
+        add_meta_box('woocommerce-customer-details', __('Dealers Customer'), [$this, 'render_customer_details_meta_box'], 'shop_order', 'normal', 'high', 10, 1);
     }
 
     //Admin Order: Add metabox content function
-    function render_customer_details_meta_box($order)
+    public function render_customer_details_meta_box($order)
     {
         global $post;
         global $wpdb;
-<<<<<<< HEAD
-        
 
-=======
->>>>>>> 9884d2ab8d3d2dd79e82291eeec4b1d7df9d22c2
         $id =  intVal($post->ID);
+
+        if (get_post_meta($id, 'customer_first_name', true)){
+            $version = 1.0;
+        }   
+
         $customer = $wpdb->get_results($wpdb->prepare("SELECT * FROM `wp_dealer_customers` WHERE `order_id` = '$id'"));
         $warranty = $wpdb->get_results($wpdb->prepare("SELECT * FROM `wp_user_warranties` WHERE `order_id` = '$id'"));
 
-<<<<<<< HEAD
-        $first .= $customer[0]->customer_first_name;
-        $last .= $customer[0]->customer_last_name;
-        $email .= $customer[0]->customer_email;
-        $address .= $customer[0]->customer_address;
-        $customer_ID .= $customer[0]->customer_user_id;
+        $first = $customer[0]->customer_first_name ?: get_post_meta($id, 'customer_first_name', true);
+        $last = $customer[0]->customer_last_name ?: get_post_meta($id, 'customer_last_name', true);
+        $email = $customer[0]->customer_email ?: get_post_meta($id, 'customer_email', true);
+        $customer_ID = $customer[0]->customer_user_id ?: null;
+        $address = $customer[0]->customer_address ?: get_post_meta($id, 'customer_country', true) . 
+                    get_post_meta($id, 'customer_street_address', true) . get_post_meta($id, 'customer_town', true) . 
+                    get_post_meta($id, 'customer_state', true)  .  get_post_meta($id, 'customer_zip', true);
+        
 
 
         if ($warranty[0] == Null){
@@ -108,90 +91,58 @@ class Order
             return $matchedUser;
         }
 
-
+        $html = NULL;
         $html .= '<div style="width: 50%; display: inline-block; vertical-align: top;" id="order_data" class="order_data_column">';
         $html .= get_user_initials($first, $last);
         $html .= $warrantyAlert;
         $html .= get_matched_user($customer_ID);
-=======
-        if ($warranty[0] == Null){
-            $warrantyAlert = '<span class="warrantyAlert noserial">No Serial Active</span>';
-        } else if ($warranty[0]->claimed_at != Null) {
-            $warrantyAlert = '<span class="warrantyAlert claimed">Warranty Claimed</span>';
-        } else if ($warranty[0]->claimed_at == Null) {
-            $warrantyAlert = '<span class="warrantyAlert unclaimed">Warranty Not Claimed</span>';
-        }
-
-        $first .= $customer[0]->customer_first_name;
-        $last .= $customer[0]->customer_last_name;
-        $email .= $customer[0]->customer_email;
-        $address .= $customer[0]->customer_address;
-        $customer_ID .= $customer[0]->customer_user_id;
-
-
-        $html .= '<div style="width: 50%; display: inline-block; vertical-align: top;" id="order_data" class="order_data_column">';
-        $html .= '<span class="customer-avatar">' . substr($first, 0, 1) . ' ' . substr($last, 0, 1) . '</span>';
-        $html .= $warrantyAlert;
-        $html .= '<p>Account ID: ' . $customer_ID . '</p>';
->>>>>>> 9884d2ab8d3d2dd79e82291eeec4b1d7df9d22c2
         $html .= '</div>';
 
         $html .= '<div style="width: 50%; display: inline-block;" id="order_data" class="order_data_column">';
         $html .= '<h3>Billing</h3>';
         $html .= '<p>First Name: ' . $first . '</p>';
-<<<<<<< HEAD
         $html .= '<p>Last Name: ' . $last . '</p>';
         $html .= '<p>Email: ' . $email . '</p>';
         $html .= '<p>Address: ' . $address . '</p>';
         $html .= '</div>';
 
         $html .= alert_customer($warrantyClaimed, $warranty);
-=======
-        $html .= '<p>Last: Name' . $last . '</p>';
-        $html .= '<p>Email: ' . $email . '</p>';
-        $html .= '<p>Address: ' . $address . '</p>';
-        $html .= '</div>';
->>>>>>> 9884d2ab8d3d2dd79e82291eeec4b1d7df9d22c2
         echo $html;
         ?>
         <style>
-        .customer-avatar{
-<<<<<<< HEAD
-            font-size: 12px;
-            background-color: #efefef;
-            padding: 20px;
-            text-align: center;
-            height: 60px;
-            font-weight: 900;
-            width: 60px;
-            color: #cacaca;
-            display: block;
-            border-radius: 50%;
-=======
-            font-size: 18px; background-color: #efefef; padding: 20px; font-weight: 900; color: #cacaca; display: block; width: fit-content; border-radius: 50%;
->>>>>>> 9884d2ab8d3d2dd79e82291eeec4b1d7df9d22c2
-        }
-        .warrantyAlert {
-            background-color: #efefef;
-            padding: 5px 10px;
-            color: #cacaca;
-            display: block;
-            width: fit-content;
-            border-radius: 5px;
-            margin: 5px 0px 5px -5px !important;
-        }
-        .warrantyAlert.unclaimed{
-            background-color: #ffeddb !important;
-            color: #ffa330 !important;
-        }
-        .warrantyAlert.claimed {
-            background-color: #d0ffe4 !important;
-            color: #2ecc71 !important;
-        }
-        .warrantyAlert.noserial {
-            background-color: #cbe3ff !important;
-            color: #167df0 !important;
-        }
+            .customer-avatar{
+                font-size: 12px;
+                background-color: #efefef;
+                padding: 20px;
+                text-align: center;
+                height: 60px;
+                font-weight: 900;
+                width: 60px;
+                color: #cacaca;
+                display: block;
+                border-radius: 50%;
+            }
+            .warrantyAlert {
+                background-color: #efefef;
+                padding: 5px 10px;
+                color: #cacaca;
+                display: block;
+                width: fit-content;
+                border-radius: 5px;
+                margin: 5px 0px 5px -5px !important;
+            }
+            .warrantyAlert.unclaimed{
+                background-color: #ffeddb !important;
+                color: #ffa330 !important;
+            }
+            .warrantyAlert.claimed {
+                background-color: #d0ffe4 !important;
+                color: #2ecc71 !important;
+            }
+            .warrantyAlert.noserial {
+                background-color: #cbe3ff !important;
+                color: #167df0 !important;
+            }
         </style>
         <?php
     }
